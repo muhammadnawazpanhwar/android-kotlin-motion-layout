97c80020df18db2b1f213547e35728d6447a8812
Muhammad Nawaz <muhammadnawazpanhwar7@gmail.com>
2022-01-12 22:21:04 +0500

    complete

---
+++ app/src/main/java/com/example/androidkotlinmotionlayout/Step1Activity.kt
@@ -0,0 +1,11 @@
+package com.example.androidkotlinmotionlayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Step1Activity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_step1_completed)
+    }
+}
